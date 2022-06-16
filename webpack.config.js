fb9b8469d0201a527927ffd1418b7d8ffe9860cb
Seth Shoemaker <Shoemakerseth2003@gmail.com>
2022-06-15 20:50:35 -0400

    set up sass webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,17 @@
-module.exports
+const path = require("path");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist')
+    },
+    module: {
+        rules: [
+            {
+                'test': /\.scss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            }
+        ]
+    }
+}
