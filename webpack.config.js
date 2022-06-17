b71743043302e6216289cc2b4010aae629107127
Seth Shoemaker <Shoemakerseth2003@gmail.com>
2022-06-17 11:43:48 -0400

    prod
    added babel and loader, made mode production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    mode:'development',
+    mode:'production',
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
@@ -12,6 +12,10 @@ module.exports = {
             {
                 'test': /\.scss$/i,
                 use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                'test': /\.js$/i,
+                use: ['babel-loader'],
             }
         ]
     },
